a4b417f5a541085803620d145ca2e9ce8f8c2780
dojo2022-toyoyama <103239377+dojo2022-toyoyama@users.noreply.github.com>
2022-06-13 17:12:00 +0900

    Update AchievementDAO.java

---
+++ src/src/dao/AchievementDAO.java
@@ -0,0 +1,65 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Achievement;
+
+public class AchievementDAO {
+	public List<Achievement> select() {
+		Connection conn = null;
+		List<Achievement> achieveList = new ArrayList<Achievement>();
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo_db/Sol-ty", "sa", "");
+
+			// SQL文を準備する<<ここを改造>>
+			String sql = "select * from Achievement";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする<<ここを改造>>
+						while (rs.next()) {
+							Achievement achieve = new Achievement(
+							rs.getInt("ACHIEVEID"),
+							rs.getString("CONDITION"),
+							rs.getString("ACHIEVECONTENT")
+							);
+							achieveList.add(achieve);
+						}
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+						achieveList = null;
+					}
+					catch (ClassNotFoundException e) {
+						e.printStackTrace();
+						achieveList = null;
+					}
+					finally {
+						// データベースを切断
+						if (conn != null) {
+							try {
+								conn.close();
+							}
+							catch (SQLException e) {
+								e.printStackTrace();
+								achieveList = null;
+							}
+						}
+					}
+			return achieveList;
+	}
+
+}
