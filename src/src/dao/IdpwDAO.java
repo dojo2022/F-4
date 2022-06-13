6d152df7b9136da0fc1d9e59abb9f9fa5058b849
dojo2022-okamoto <103237383+dojo2022-okamoto@users.noreply.github.com>
2022-06-13 16:23:53 +0900

    ログイン関連作成

--- src/src/dao/IdpwDAO.java
+++ src/src/dao/IdpwDAO.java
@@ -1,5 +1,168 @@
 package dao;
 
-public class IdpwDAO {
+import java.security.MessageDigest;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import model.Idpw;
+import model.LoginUser;
+
+public class IdpwDAO {
+	// ログインできるならtrueを返す
+		public LoginUser isLoginOK(Idpw idpw) {
+			Connection conn = null;
+			LoginUser user = null;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo_db/Sol-ty", "sa", "");
+
+				// SELECT文を準備する
+				String sql = "select count(*), USERID, USERNAME, TASKCOUNT, VOICESWITCH, COMPVOICE, INCOMPVOICE, ACVOICE, BGICONTENT FROM IDPW LEFT OUTER JOIN VOICE ON IDPW.VOICESELECT = VOICE.VOICESELECT LEFT OUTER JOIN BGI ON IDPW.BGISELECT = BGI.BGISELECT where USERNAME = ? and PASSWORD = ? GROUP BY USERID";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				String result = getHash(idpw.getPassword(),"MD5");//MD5でハッシュ化
+				pStmt.setString(1, idpw.getUsername());
+				pStmt.setString(2,result);
+
+				// SELECT文を実行し、結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+
+				// ユーザーIDとパスワードが一致するユーザーがいたかどうかをチェックする
+				rs.next();
+				if (rs.getInt("count(*)") == 1) {
+					user =  new LoginUser(rs.getInt("USERID"),
+							rs.getString("USERNAME"),
+							rs.getInt("TASKCOUNT"),
+							rs.getInt("VOICESWITCH"),
+							rs.getString("COMPVOICE"),
+							rs.getString("INCOMPVOICE"),
+							rs.getString("ACVOICE"),
+							rs.getString("BGICONTENT"));
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+				user = null;
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				user = null;
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+						user = null;
+					}
+				}
+			}
+
+			// 結果を返す
+			return user;
+		}
+		public boolean insert(Idpw idpw) {
+			Connection conn = null;
+			boolean insertResult = false;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo_db/Sol-ty", "sa", "");
+
+				// SELECT文を準備する
+				String sql = "insert into idpw (USERNAME, PASSWORD, SECQUESTION, SECANSWER, VOICESWITCH, VOICESELECT, BGISELECT) values (?, ?, ?, ?, ?, ?, ?, ?)";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+				String username = idpw.getUsername();
+				String password = idpw.getPassword();
+			    String secques = idpw.getSecquestion();
+				String secans = idpw.getSecanswer();
+
+				password = getHash(password, "MD5");
+
+				String sql2 = "select count(*) from idpw where USERNAME = ?";
+				PreparedStatement pStmt2 = conn.prepareStatement(sql2);
+				pStmt2.setString(1, idpw.getUsername());
+
+				// SELECT文を実行し、結果表を取得する
+				ResultSet rs = pStmt2.executeQuery();
+				rs.next();
+				// ユーザーネームが一致するユーザーがいたかどうかをチェックする
+				if (rs.getInt("count(*)") == 1) {
+					return false;
+				}
+
+
+				if (username != null && !username.equals("")) pStmt.setString(1, username);
+				else return false;
+				if (password != null && !password.equals("")) pStmt.setString(2, password);
+				else return false;
+				if (secques != null && !secques.equals("")) pStmt.setString(3, secques);
+				else return false;
+				if (secans != null && !secans.equals("")) pStmt.setString(4, secans);
+				else return false;
+				pStmt.setInt(5, 1);
+				pStmt.setInt(6, 1);
+				pStmt.setInt(7, 1);
+
+				if (pStmt.executeUpdate() == 1) {
+					return true;
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+				insertResult = false;
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				insertResult = false;
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+						insertResult = false;
+					}
+				}
+			}
+
+			// 結果を返す
+			return insertResult;
+		}
+		//複数のハッシュアルゴリズムに対応しています。
+		//マルチハッシュ化メソッドです。
+		public String getHash(String str, String algo) {
+			byte[] cipher_byte;
+			try{
+				MessageDigest md = MessageDigest.getInstance(algo);
+				md.update(str.getBytes());
+				cipher_byte = md.digest();
+				StringBuilder sb = new StringBuilder(2 * cipher_byte.length);
+				for(byte b: cipher_byte) {
+					sb.append(String.format("%02x", b&0xff) );
+				}
+				return sb.toString();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return "";
+		}
 }
--- src/src/servlet/LoginServlet.java
+++ src/src/servlet/LoginServlet.java
@@ -1,11 +1,18 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.IdpwDAO;
+import model.Idpw;
+import model.LoginUser;
 
 /**
  * Servlet implementation class LoginServlet
@@ -18,16 +25,45 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		if(session.getAttribute("user") == null) {
+			// ログインページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+			dispatcher.forward(request, response);
+		}
+		else {
+			response.sendRedirect("/ListServlet");
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String username = request.getParameter("USERNAME");
+		String password = request.getParameter("PASSWORD");
+
+		// ログイン処理を行う
+		IdpwDAO iDao = new IdpwDAO();
+
+		LoginUser user = iDao.isLoginOK(new Idpw(username, password, "", ""));
+		if (user != null) {	// ログイン成功
+			// セッションスコープにIDを格納する
+			HttpSession session = request.getSession();
+			session.setAttribute("user", user);
+
+			// リストサーブレットにリダイレクトする
+			response.sendRedirect("/Sol_ty/ListServlet");
+		}
+		else {// ログイン失敗
+			// ログインページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+			dispatcher.forward(request, response);
+		}
+
+
 	}
 
 }
