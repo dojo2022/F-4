80c7e9b265f1669e163f32bcd15fe13764371552
dojo2022-fukada <103239281+dojo2022-fukada@users.noreply.github.com>
2022-06-14 16:19:07 +0900

    BgiUploadServlet generate

--- src/src/servlet/SettingServlet.java
+++ src/src/servlet/SettingServlet.java
@@ -1,11 +1,14 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class SettingServlet
@@ -20,6 +23,16 @@ public class SettingServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+				HttpSession session = request.getSession();
+				if (session.getAttribute("username") == null) {
+					response.sendRedirect("/Sol_ty/LoginServlet");
+					return;
+				}
+
+				// 設定ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/setting.jsp");
+			dispatcher.forward(request, response);
 	}
 
 	/**
@@ -28,6 +41,21 @@ public class SettingServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+				HttpSession session = request.getSession();
+				if (session.getAttribute("username") == null) {
+					response.sendRedirect("/Sol_ty/LoginServlet");
+					return;
+				}
+
+				// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+				int userid = request.getParameter("NUMBER");
+				String postalcode = request.getParameter("POSTALCODE");
+				String address = request.getParameter("ADDRESS");
+				String office = request.getParameter("OFFICE");
+		
 	}
 
 }
