9e4825c06f1b4cae785082b4acb7b839c377fc5b
dojo2022-maedayuuki <103239760+dojo2022-maedayuuki@users.noreply.github.com>
2022-06-15 15:13:29 +0900

    Update BgiUploadServlet.java

--- src/src/servlet/BgiUploadServlet.java
+++ src/src/servlet/BgiUploadServlet.java
@@ -1,11 +1,18 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.VoiceBgiDAO;
+import model.LoginUser;
+import model.Upload;
 
 /**
  * Servlet implementation class BgiUploadServlet
@@ -13,29 +20,55 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/BgiUploadServlet")
 public class BgiUploadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BgiUploadServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public BgiUploadServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user") == null) {
+			response.sendRedirect("/Sol_ty/LoginServlet");
+			return;
+		}
+		//3つ使う
+		LoginUser user = (LoginUser)session.getAttribute("user");
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		int userid = user.getUserid();
+		String bgiupload = request.getParameter("BGIUPLOAD");
+		String bgititle = request.getParameter("BGITITLE");
+
+
+		// 更新または削除を行う
+		VoiceBgiDAO VDao = new VoiceBgiDAO();
+
+		if (VDao.upload(new Upload(userid,bgiupload,bgititle))) {	// アップロード成功
+			request.setAttribute("result",
+					"アップロード成功");
+		}
+		else {												// アップロード失敗
+			request.setAttribute("result",
+					"アップロード失敗");
+		}
+
+
+
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
+
+
